3e52adaee90f5fe8a76a34cd14e785e6b01deaaf
Nabiul-110 <faisalnabi350@gmail.com>
2025-09-20 01:24:04 +0600

    first

--- newapplication/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-class Test
-{
-  public static void Main(string[] args)
-    {
-        Console.WriteLine(123);
-    }
-}
---
+++ newone/Program.cs
@@ -0,0 +1,28 @@
+﻿/*for 1st one
+name:iphone 14
+price:$340.5
+category:smart phone
+available:true
+sold:5 */
+
+using System;
+
+class Test
+{
+  public static void Main(string[] args)
+    {
+        string prouduct,category;
+        prouduct ="iphone 14";
+        category ="smart phone";
+        double pr= 340.4;
+        bool avail = true;
+        Console.Write("Name:");
+        Console.WriteLine(prouduct);
+        Console.WriteLine("price:",pr);
+        Console.WriteLine("category:",category);
+        Console.WriteLine("available:",avail);
+        Console.WriteLine("sold:5");
+
+    }
+}
+
